bfad8573dca6d033bd64a80e11e83501d36d68f6
NickPresley <nicholas.presley@student.allenisd.org>
2021-11-10 12:06:53 -0600

    a

---
+++ Home-Page/scripts/js.js
@@ -0,0 +1,8 @@
+function enterName() {
+    xnm = document.getElementById("name_").innerHTML
+    xnu = document.getElementById("num_").innerHTML
+    xps = document.getElementById("pass_").innerHTML
+    tbl = document.getElementById("table")
+    ctr = document.createElement("tr")
+    cus
+}
--- login/scripts/sdkdfiDFIdsfiJDSFOIgj8&fs863uh3974357.js
+++ login/scripts/sdkdfiDFIdsfiJDSFOIgj8&fs863uh3974357.js
@@ -1,24 +1,9 @@
-var logins = [{
-	"username": "foody9000", "password": "Lala0310"
-}, {
-	"username1": "brayden", "password": "Lala0310"
-}]
-
-function getUsername(code) {
-	return logins.filter(
-	  function(logins) {
-		return logins.code == code
-	  }
-	);
-  }
-  user_ = document.getElementById("user_").value;
-  pass_ = document.getElementById("Pass_").value;
-  var found = getPassword(user_);
-
 function login() {
+	user_ = document.getElementById("user_").value;
+	pass_ = document.getElementById("Pass_").value;
 	if (user_.toLowerCase() == "foody9000") {
-		if (pass_ == found) {
-			var x = alert("Successfully Logged In");
+		if (pass_ == "Lala0310") {
+			alert("Successfully Logged In");
 			window.location.href = "../Home-Page/html.html";
 		} else {
 			alert("Wrong Username Or Password");
