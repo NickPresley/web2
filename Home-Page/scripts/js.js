800b89260b4bd958a035ccbe315cc86b1168523a
NickPresley <nicholas.presley@student.allenisd.org>
2021-11-11 08:04:20 -0600

    Update js.js

--- Home-Page/scripts/js.js
+++ Home-Page/scripts/js.js
@@ -1,8 +1,20 @@
 function enterName() {
-    xnm = document.getElementById("name_").innerHTML
-    xnu = document.getElementById("num_").innerHTML
-    xps = document.getElementById("pass_").innerHTML
-    tbl = document.getElementById("table")
-    ctr = document.createElement("tr")
-    cus
+    xnm = document.getElementById("name_").innerText;
+    xnu = document.getElementById("num_").innerText;
+    xps = document.getElementById("pass_").innerText;
+    ctr = document.createElement("tr");
+    ctr.id = "ctr-";
+    document.getElementById("table").appendChild(ctr);
+    ctr1 = document.createElement("th");
+    ctr2 = document.createElement("th");
+    ctr3 = document.createElement("th");
+    ctr1.id = "ctr1";
+    ctr2.id = "ctr2";
+    ctr3.id = "ctr3";
+    document.getElementById("ctr-").appendChild(ctr1);
+    document.getElementById("ctr-").appendChild(ctr2);
+    document.getElementById("ctr-").appendChild(ctr3);
+    document.getElementById("ctr1").innerText = xnm
+    document.getElementById("ctr2").innerText = xnu
+    document.getElementById("ctr3").innerText = xps
 }
